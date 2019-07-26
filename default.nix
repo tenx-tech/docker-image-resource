00f29d6e18615fe8699da8c11b487c63c0ab0e97
Trinh Hoang Anh <anh@tenx.tech>
2019-07-26 18:50:00 +0800

    Build docker image with nix

---
+++ default.nix
@@ -0,0 +1,40 @@
+{ buildGoPackage, fetchgitLocal, dockerTools, lib, docker, bash, busybox, cacert, jq, runCommand }:
+  # slug = dockerTools.buildImage {
+  #   inherit name;
+  #   tag = "latest";
+  #   fromImage = null;
+  #   contents = [ bin bash cacert ];
+  #   config = {
+  #     Cmd = [ "/bin/${name}" ];
+  #   };
+  # };
+let
+  check = buildGoPackage {
+    name = "concourse-docker-image-resource-check";
+    goPackagePath = "github.com/concourse/docker-image-resource";
+    subPackages = [ "cmd/check" "cmd/print-metadata" "vendor/github.com/awslabs/amazon-ecr-credential-helper/ecr-login/cmd"];
+
+    src = lib.cleanSourceWith {
+      filter = name: type: !lib.hasSuffix ".nix" name;
+      src = lib.cleanSource ./.;
+    };
+  };
+
+  resources = runCommand "cp-resources-to-opt" { }  ''
+    mkdir -p $out/opt/resource
+    cd $out/opt/resource
+    cp -r ${check}/bin/. ./.
+    cp -r ${./assets}/. ./.
+    patchShebangs .
+
+    mkdir -p $out/bin
+    cd $out/bin
+    cp ${check}/bin/cmd ./docker-credential-ecr-login
+  '';
+
+  image = dockerTools.buildLayeredImage {
+    name = "concourse-docker-image-resource";
+    tag = "latest";
+    contents = [ busybox cacert resources jq docker bash ];
+  };
+in image
